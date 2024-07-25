0e321af499311254db7d56186493275a69c4e749
Leandro Juan <leandrojuan88@gmail.com>
2024-07-25 00:08:10 -0300

    correcto

---
+++ testEmail.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+// import nodemailer from "nodemailer";
+
+const userGmail = "leandrojuan88@gmail.com";
+const passAppGmail = "zcdu ywny dfyi kthq";
+
+// Set up Nodemailer transporter
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: userGmail,
+    pass: passAppGmail,
+  },
+});
+
+// Define a route for sending emails
+// Set up email options
+const mailOptions = {
+  from: userGmail,
+  to: userGmail,
+  subject: "Enviado con Exito!!",
+  text: "Te damos la Bienvenida a Email...",
+};
+
+// Send email
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log("Email sent: " + info.response);
+});
